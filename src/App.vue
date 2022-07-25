3aa19111e6a7a7bab1760da293094fa5a98971f4
maggot_code <wowhuanhuan233@163.com>
2022-07-25 13:47:35 +0800

    feat :-1:

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,15 @@
  * @Author: maggot-code
  * @Date: 2022-07-25 09:17:33
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-07-25 09:36:21
+ * @LastEditTime: 2022-07-25 13:45:02
  * @Description: 
 -->
 <script setup>
-// import GridContainer from '@/biz/Grid/view/GridContainer.vue';
-// import BizModules from "@/config/module.conf.js";
-import { useUser } from "@/biz/User";
-
-const admin = useUser();
-console.log(admin);
 </script>
 
 <template>
     <div class="map-container">
-        <!-- <template v-for="(row, index) in BizModules">
-            <GridContainer :index="index" :name="row.name" :module="row.module">
-                <component :is="row.component"></component>
-            </GridContainer>
-        </template> -->
+        <h1>App</h1>
     </div>
 </template>
 
---
+++ src/pages/homeindex/homeindex.vue
@@ -0,0 +1,17 @@
+<!--
+ * @FilePath: \vue2.7_workflow\src\pages\homeindex\homeindex.vue
+ * @Author: maggot-code
+ * @Date: 2022-07-25 13:46:02
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2022-07-25 13:46:33
+ * @Description: 防汛作战大屏容器
+-->
+<script setup></script>
+
+<template>
+    <h1>home index</h1>
+</template>
+
+<style scoped lang='scss'>
+@import "./homeindex.scss";
+</style>

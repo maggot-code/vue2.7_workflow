ca9bf45bf1e39dad07dfb28943a9915ca9d43098
maggot_code <wowhuanhuan233@163.com>
2022-07-25 14:59:18 +0800

    feat :-1:

---
+++ src/composable/User/index.js
@@ -0,0 +1,8 @@
+/*
+ * @FilePath: \vue2.7_workflow\src\composable\User\index.js
+ * @Author: maggot-code
+ * @Date: 2022-07-25 14:54:24
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2022-07-25 14:55:27
+ * @Description:
+ */
---
+++ src/composable/User/usecase/useUserLogin.js
@@ -0,0 +1,11 @@
+/*
+ * @FilePath: \vue2.7_workflow\src\composable\User\usecase\useUserLogin.js
+ * @Author: maggot-code
+ * @Date: 2022-07-25 14:56:11
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2022-07-25 14:56:40
+ * @Description:
+ */
+export function useUserLogin() {}
+
+export default useUserLogin;
